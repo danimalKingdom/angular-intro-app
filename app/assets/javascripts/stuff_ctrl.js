976d686f0155ae5d457ccbc80af1c2c45a21a5bb
dani <dani.zaghian@gmail.com>
2017-02-26 11:18:56 -0800

    ng-repeat

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -2,9 +2,8 @@
   "use strict";
 
   angular.module("app").controller("stuffCtrl", function($scope) {
-      $scope.firstTask = "Walk the cat";
-      $scope.secondTask = "Feed the cat";
-      $scope.thirdTask = "Pet the cat";
+      $scope.message = "hello world";
+      $scope.tasks = ["Walk the cat", "Feed the cat", "Pet the cat"];
       window.$scope = $scope;
   });
 }());
