0ca446e016f5c6ed3bf534b933542f2ec6a6275f
dani <dani.zaghian@gmail.com>
2017-02-26 11:38:40 -0800

    ng-click

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -4,6 +4,11 @@
   angular.module("app").controller("stuffCtrl", function($scope) {
       $scope.message = "hello world";
       $scope.tasks = ["Walk the cat", "Feed the cat", "Pet the cat"];
+
+      $scope.addTask = function(newTask){
+        $scope.tasks.push(newTask);
+      };
+
       window.$scope = $scope;
   });
 }());
