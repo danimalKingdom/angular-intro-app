677488442f3d049908387d25b4ee751ebf3e994e
dani <dani.zaghian@gmail.com>
2017-02-26 12:05:31 -0800

    cleared text box, validation, disappearing task on click

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -5,8 +5,17 @@
       $scope.message = "hello world";
       $scope.tasks = ["Walk the cat", "Feed the cat", "Pet the cat"];
 
+      //if the input is blank (""), don't submit
+      //submit if truthy
       $scope.addTask = function(newTask){
-        $scope.tasks.push(newTask);
+        if (newTask) {
+          $scope.tasks.push(newTask);
+          $scope.newTask = null;
+        }
+      };
+
+      $scope.completeTask = function(index) {
+        $scope.tasks.splice(index, 1);
       };
 
       window.$scope = $scope;
