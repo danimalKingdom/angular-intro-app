5e9f8215b9642a7a8c77a24745ca7a0f7385f179
dani <dani.zaghian@gmail.com>
2017-02-26 11:03:58 -0800

    added tasks

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -2,7 +2,9 @@
   "use strict";
 
   angular.module("app").controller("stuffCtrl", function($scope) {
-      $scope.message = "Hello World!";
+      $scope.firstTask = "Walk the cat";
+      $scope.secondTask = "Feed the cat";
+      $scope.thirdTask = "Pet the cat";
       window.$scope = $scope;
   });
 }());
